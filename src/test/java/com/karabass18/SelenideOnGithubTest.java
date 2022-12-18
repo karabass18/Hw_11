f4a3746c463b0e2ba4122d2d084ebef0037edcc5
Aleksei Dementev <karabass18@gmail.com>
2022-12-18 14:29:13 +0500

    solve with sleep(200)

--- src/test/java/com/karabass18/SelenideOnGithubTest.java
+++ src/test/java/com/karabass18/SelenideOnGithubTest.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class SelenideOnGithubTest {
     @BeforeEach
     public void startSet() {
