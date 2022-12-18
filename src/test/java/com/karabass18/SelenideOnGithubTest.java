8b16f22dc13bf5a23bb8fb06222c3898375378aa
Aleksei Dementev <karabass18@gmail.com>
2022-12-18 14:28:13 +0500

    solve with sleep(200)

---
+++ src/test/java/com/karabass18/SelenideOnGithubTest.java
@@ -0,0 +1,38 @@
+package com.karabass18;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideOnGithubTest {
+    @BeforeEach
+    public void startSet() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        sleep(200);
+        $(".js-wiki-more-pages-link").click();
+
+    }
+
+    @Test
+    //@Disabled
+    public void wikiHasSoftAssertionsTest() {
+
+        $("#wiki-pages-box.wiki-pages-box").shouldHave(text("SoftAssertions"));
+
+    }
+
+    @Test
+    //@Disabled
+    public void softAssertionHasJunit() {
+        //sleep (5000);
+        $("#wiki-pages-box.wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").shouldBe(visible);
+
+    }
+}
